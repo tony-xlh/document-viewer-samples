1bdb670a2565807a691621a16241f769c368c55c
xulihang <xulihanghai@163.com>
2024-10-11 14:52:30 +0800

    implement the vue demo

--- frameworks/vue/pdf-app/src/App.vue
+++ frameworks/vue/pdf-app/src/App.vue
@@ -1,30 +1,52 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { DDV, UiConfig } from 'dynamsoft-document-viewer';
+import "dynamsoft-document-viewer/dist/ddv.css";
+import { onMounted, ref } from 'vue';
+const initialized = ref(false);
+let editViewer;
+
+const initDDV = async () => {
+  DDV.Core.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ=="; // Public trial license which is valid for 24 hours
+  DDV.Core.engineResourcePath = "assets/ddv-resources/engine";// Lead to a folder containing the distributed WASM files
+  await DDV.Core.loadWasm();
+  await DDV.Core.init(); 
+  // Configure image filter feature which is in edit viewer
+  DDV.setProcessingHandler("imageFilter", new DDV.ImageFilter());
+  const config = DDV.getDefaultUiConfig("editViewer", {includeAnnotationSet: true}) as UiConfig;
+  // Create an edit viewer
+  editViewer = new DDV.EditViewer({
+    container: "container",
+    uiConfig: config,
+  });
+  initialized.value = true;
+}
+
+onMounted(()=>{
+  if (initialized.value === false) {
+    initDDV();
+  }
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h2>Document Viewer Demo</h2>
+    <div v-if="!initialized">Initializing...</div>
+    <div id="container"></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+#app {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#container {
+  max-width: 80%;
+  width: 1280px;
+  height: 480px;
 }
 </style>
