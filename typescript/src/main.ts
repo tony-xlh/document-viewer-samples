3c18fa4e2a2a124b8c99ae8952da20daf156eb85
xulihang <xulihanghai@163.com>
2025-06-04 13:44:34 +0800

    add typescript demo

---
+++ typescript/src/main.ts
@@ -0,0 +1,22 @@
+import './style.css'
+import { DDV } from 'dynamsoft-document-viewer';
+
+DDV.on("error", e => { console.log(e.cause); });
+DDV.on("warning", e => { console.log(e.cause); });
+(window as any)["DDV"] = DDV;
+
+await (async () => {
+  DDV.Core.license = "t0115uQAAAJJfo6BBRS2nKlciU3D43dj7VHCcttUcCvbCsPEBUWPpoKVg/3acGSopnBBrweZotGHlXWpXlwd0xCvWJDxjF33PYb3Nj6Xch9NZQI4NwWEOcUSHNsBGxsvrG3poM3UbgFq5VfUOPYqYqr4BIl0nmw==";
+  DDV.Core.engineResourcePath = "assets/ddv-resources/engine";
+  DDV.Core.loadWasm();
+  await DDV.Core.init();
+})();
+
+
+DDV.setProcessingHandler("imageFilter", new DDV.ImageFilter());
+
+const editViewer = new DDV.EditViewer({
+  container: "container",
+});
+
+(window as any)["editViewer"] = editViewer;
